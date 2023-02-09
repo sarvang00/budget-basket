1d3c6d4d05ca25438cbce60628a1d0dc8946a9e9
Jayant Patidar <jy862746@dal.ca>
2023-02-09 19:53:21 -0400

    first commit, just initializer project

---
+++ Financial-Economizer-Backend/src/test/java/com/dal/economizer/FinancialEconomizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dal.economizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialEconomizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
