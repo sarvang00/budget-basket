6233deefbf57fb9d3e3bdc451360a63557df1cb0
Jayant Patidar <jy862746@dal.ca>
2023-02-09 20:48:48 -0400

    implemented User Entity class

--- Financial-Economizer-Backend/src/main/java/com/dal/economizer/repository/UserRepository.java
+++ Financial-Economizer-Backend/src/main/java/com/dal/economizer/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.dal.economizer.repository;
 
+import com.dal.economizer.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User,Integer> {
 
 
 }
