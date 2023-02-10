fca997c4305a2bbc9aef222985e3bc061537df20
Jayant Patidar <jy862746@dal.ca>
2023-02-09 23:07:54 -0400

    modified User model class, changed fields,added new fields for updating user table structure in db

--- Financial-Economizer-Backend/src/main/java/com/dal/economizer/model/User.java
+++ Financial-Economizer-Backend/src/main/java/com/dal/economizer/model/User.java
@@ -2,15 +2,20 @@ package com.dal.economizer.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "user_name")
-    private String name;
+    @Column(name = "user_first_name")
+    private String firstName;
+
+    @Column(name = "user_last_name")
+    private String lastName;
 
     @Column(name = "user_email", unique = true)
     private String email;
@@ -18,12 +23,26 @@ public class User {
     @Column(name = "user_password")
     private String password;
 
-    public User() {}
+    @Column(name = "user_phone")
+    private String phoneNumber;
 
-    public User(String name, String email, String password) {
-        this.name = name;
+    @Column(name = "user_created", updatable = false)
+    private Date createdAt=new Date();
+
+    @Column(name = "user_updated")
+    private Date updatedAt;
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String email, String password, String phoneNumber, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public Long getId() {
@@ -34,12 +53,20 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -58,8 +85,41 @@ public class User {
         this.password = password;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
     @Override
     public String toString() {
-        return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + "]";
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
     }
 }
