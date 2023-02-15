c0e1f636a92c41a1cf8108a1bacfc5f397087dcf
Brahmdeep Aneja <br597934@dal.ca>
2023-02-14 23:46:32 -0400

    Product fetch functionality to show on home page etc

--- src/main/java/com/dal/economizer/model/Product.java
+++ src/main/java/com/dal/economizer/model/Product.java
@@ -11,7 +11,37 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    private String description;
+
+    private Double discount_price;
+
+    private Double actual_price;
     private Date expiryDate;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getDiscount_price() {
+        return discount_price;
+    }
+
+    public void setDiscount_price(Double discount_price) {
+        this.discount_price = discount_price;
+    }
+
+    public Double getActual_price() {
+        return actual_price;
+    }
+
+    public void setActual_price(Double actual_price) {
+        this.actual_price = actual_price;
+    }
 
     public Long getId() {
         return id;
--- src/main/java/com/dal/economizer/service/ProductService.java
+++ src/main/java/com/dal/economizer/service/ProductService.java
@@ -3,29 +3,18 @@ package com.dal.economizer.service;
 import com.dal.economizer.dao.ProductDao;
 import com.dal.economizer.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService {
+    @Autowired
     private ProductDao productDao;
 
-    @Autowired
-    public ProductService(ProductDao productDao) {
-        this.productDao = productDao;
+    public List<Product> getAllProducts(){
+        return (List<Product>) productDao.findAll();
     }
 
-    @Scheduled(cron = "0 0 0/2 * * *")
-    public void sendExpiryNotifications() {
-        List<Product> products = productDao.findAll();
-        for (Product product : products) {
-            long timeDifference = product.getExpiryDate().getTime() - System.currentTimeMillis();
-            if (timeDifference <= 2 * 24 * 60 * 60 * 1000) {
-                System.out.println(product.getName() + " is going to expire, you may wanna consume it and restock it!");
-            }
-        }
-    }
 }
 
