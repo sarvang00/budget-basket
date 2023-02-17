5245ef1b23e8ee4e803543a762304f375e60bca1
Sarvang Dave <sarvang00@gmail.com>
2023-02-16 20:14:55 -0400

    Footer becomes a component

--- BudgetBasket/src/app/app.module.ts
+++ BudgetBasket/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { FinmanagerModule } from './finmanager/finmanager.module';
 import { StoreModule } from './store/store.module';
 
 import { UserManagementModule } from './user-management/user-management.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
