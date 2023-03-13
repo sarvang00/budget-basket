03816821da18d142b96a62d6b7336a73e4d5bbac
Devansh Khanna <dv700702@dal.ca>
2023-03-13 00:28:57 -0300

    save filrs

---
+++ src/test/java/com/dal/PFE/controller/UserControllerTest.java
@@ -0,0 +1,12 @@
+package com.dal.PFE.controller;
+
+import org.apache.catalina.User;
+import org.junit.jupiter.api.Test;
+
+public class UserControllerTest {
+    @Test
+
+
+    void name() {
+    }
+}
---
+++ src/test/java/com/dal/PFE/model/UserTest.java
@@ -0,0 +1,21 @@
+package com.dal.PFE.model;
+
+import org.apache.catalina.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class UserTest {
+
+
+     User testUser = new User();
+
+
+
+
+
+}
