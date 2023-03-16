f7f44a37bf03834b5f6738196e8cd903f7304d21
Brahmdeep Aneja <br597934@dal.ca>
2023-03-16 19:19:21 -0300

    Inventory.java

---
+++ backend/src/main/java/com/dal/PFE/model/Inventory.java
@@ -0,0 +1,88 @@
+package com.dal.PFE.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer serial_number;
+    private String product_name;
+    private String product_category;
+    private Double product_quantity;
+    private Double discount_price;
+    private Double actual_price;
+    private Date purchase_date;
+    private Date expiry_date;
+
+    public Integer getSerial_number() {
+        return serial_number;
+    }
+
+    public void setSerial_number(Integer serial_number) {
+        this.serial_number = serial_number;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getProduct_category() {
+        return product_category;
+    }
+
+    public void setProduct_category(String product_category) {
+        this.product_category = product_category;
+    }
+
+    public Double getProduct_quantity() {
+        return product_quantity;
+    }
+
+    public void setProduct_quantity(Double product_quantity) {
+        this.product_quantity = product_quantity;
+    }
+
+    public Double getDiscount_price() {
+        return discount_price;
+    }
+
+    public void setDiscount_price(Double discount_price) {
+        this.discount_price = discount_price;
+    }
+
+    public Double getActual_price() {
+        return actual_price;
+    }
+
+    public void setActual_price(Double actual_price) {
+        this.actual_price = actual_price;
+    }
+
+    public Date getPurchase_date() {
+        return purchase_date;
+    }
+
+    public void setPurchase_date(Date purchase_date) {
+        this.purchase_date = purchase_date;
+    }
+
+    public Date getExpiry_date() {
+        return expiry_date;
+    }
+
+    public void setExpiry_date(Date expiry_date) {
+        this.expiry_date = expiry_date;
+    }
+}
