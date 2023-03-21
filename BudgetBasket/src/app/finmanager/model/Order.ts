91a22fda9802af285c8ec3c47fc0e440cad62708
mansi <mansi.v@turabit.com>
2023-03-21 19:33:52 -0300

    Adding models for category and Order

---
+++ BudgetBasket/src/app/finmanager/model/Order.ts
@@ -0,0 +1,8 @@
+export interface OrderComponent {
+    orderDate: Date;
+    productName: string;
+    category: string;
+    quantity: number;
+    actualPrice: number;
+    discountPrice: number;
+  }
