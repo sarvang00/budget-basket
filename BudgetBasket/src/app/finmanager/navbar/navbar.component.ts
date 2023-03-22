f0c930b19d62192cfc758beab7fb396908746783
Sarvang Dave <sarvang00@gmail.com>
2023-03-21 21:54:51 -0300

    fixing user navigation in finmanager

--- BudgetBasket/src/app/finmanager/navbar/navbar.component.ts
+++ BudgetBasket/src/app/finmanager/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserManagementService } from 'src/app/services/user-management.service';
+import { User } from 'src/app/user-management/model/User';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  myAuthUser!: User;
 
+  constructor(private userService: UserManagementService, private router: Router) {
+    this.myAuthUser = userService.getAuthUser();
+  }
+
+  ngOnInit() {
+    if(!this.myAuthUser.firstName) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  logOut() {
+    this.userService.signOutUser();
+    this.router.navigate(['/']);
+  }
 }
