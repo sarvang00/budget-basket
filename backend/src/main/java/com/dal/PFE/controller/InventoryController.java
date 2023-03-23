b6dcdc0ca8449185fea2bc08479159b3bfec45d1
Jayant Patidar <jy862746@dal.ca>
2023-03-23 20:41:57 -0300

    fixed a bug in inventory controller

--- backend/src/main/java/com/dal/PFE/controller/InventoryController.java
+++ backend/src/main/java/com/dal/PFE/controller/InventoryController.java
@@ -4,12 +4,10 @@ import com.dal.PFE.dao.InventoryDao;
 import com.dal.PFE.model.Inventory;
 import com.dal.PFE.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("inventory")
 public class InventoryController {
 
