03587a8baf8f1edf39ee3af2ad821f9123ac8adf
Jayant Patidar <jy862746@dal.ca>
2023-03-23 21:44:58 -0300

    bug fixed in inventory controller during integration

--- backend/src/main/java/com/dal/PFE/controller/InventoryController.java
+++ backend/src/main/java/com/dal/PFE/controller/InventoryController.java
@@ -24,7 +24,7 @@ public class InventoryController {
     }
 
     @GetMapping({"/getInventory"})
-    public List<Inventory> getInventoryFromEmail(String email){
+    public List<Inventory> getInventoryFromEmail(@RequestBody String email){
         return inventoryService.getInventoryFromEmail(email);
     }
 }
