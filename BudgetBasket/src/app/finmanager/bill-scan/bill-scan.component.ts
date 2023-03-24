e763caabe80f68be684f6937c0ac3b6d206771be
Sarvang Dave <sarvang00@gmail.com>
2023-03-24 15:47:58 -0300

    Integrating to template and showing the scanned on screen

--- BudgetBasket/src/app/finmanager/bill-scan/bill-scan.component.ts
+++ BudgetBasket/src/app/finmanager/bill-scan/bill-scan.component.ts
@@ -14,15 +14,15 @@ export class BillScanComponent {
   imageSrc!: string;
   fileName = '';
 
-  products : any =[];
-  items: string[] = []; 
+  products: any = [];
+  items: string[] = [];
   prices: string[] = [];
   showData = false;
   scanningData = false;
 
-  constructor(private FinManagerService: FinManagerService, private router: Router) {}
+  constructor(private FinManagerService: FinManagerService, private router: Router) { }
 
-  onFileSelected(event:any): void {
+  onFileSelected(event: any): void {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -31,74 +31,66 @@ export class BillScanComponent {
     };
   }
   async onUpload(): Promise<void> {
-    console.log("*****");
+    // console.log("*****");
     this.scanningData = true;
     const result = await Tesseract.recognize(this.imageSrc);
-    console.log(result.data.lines);
+    this.imageSrc = "";
+    // console.log(result.data.lines);
 
     const result_line = result.data.lines[0];
     const all_product = [];
     let upper_details = 0;
-   
+
     for (let i = 0; i < result.data.lines.length; i++) {
       const result_line = result.data.lines[i];
-      
-      if (result_line.words[0].text == "STH") 
-      {
+
+      if (result_line.words[0].text == "STH") {
         upper_details = i;
         break;
       }
-    
+
     }
 
     let lower_details = 0;
-    for (let i = upper_details+1; i < result.data.lines.length; i++) {
+    for (let i = upper_details + 1; i < result.data.lines.length; i++) {
       const result_line = result.data.lines[i];
-      
-      if (result_line.words[0].text == "SUBTOTAL") 
-      {
+
+      if (result_line.words[0].text == "SUBTOTAL") {
         lower_details = i;
         break;
       }
-    
+
     }
 
 
-    console.log("upper_details",upper_details);
-    for (let i = upper_details+1; i < lower_details; i++) {
-      
+    // console.log("upper_details", upper_details);
+    for (let i = upper_details + 1; i < lower_details; i++) {
+
       const result_line = result.data.lines[i];
       const total_words = result_line.words.length;
-      console.log("line data : ",result_line);
-      const product_code = result_line.words[total_words-3].text;
-    
+      // console.log("line data : ", result_line);
+      const product_code = result_line.words[total_words - 3].text;
+
       let product_name = "";
-      for (let j = 0; j < total_words-3; j++) {
+      for (let j = 0; j < total_words - 3; j++) {
         product_name = product_name + result_line.words[j].text + " ";
-    
+
       }
-      console.log("product name : ",product_name);
-      const product_price = result_line.words[total_words-1].text.replace(/(\r\n|\n|\r)/gm, "");
-      const quantity = result_line.words[total_words-2].text.replace(/(\r\n|\n|\r)/gm, "");
-      console.log("product price : ",product_price); // replace this with your desired operation on each element
+      // console.log("product name : ", product_name);
+      const product_price = result_line.words[total_words - 1].text.replace(/(\r\n|\n|\r)/gm, "");
+      const quantity = result_line.words[total_words - 2].text.replace(/(\r\n|\n|\r)/gm, "");
+      // console.log("product price : ", product_price); // replace this with your desired operation on each element
       var product = {
-        productID :product_code,
-        productName:product_name,
-        productPrice :product_price
+        productID: product_code,
+        productName: product_name,
+        productPrice: product_price,
+        purchaseDate: new Date()
       };
       this.products.push(product);
-      
-    }
-    console.log(this.products);
 
+    }
+    // console.log(this.products);
     this.showData = true;
   }
-
-  // onClickCompletedScanning() 
-  // {
-  //   this.emailid = data.emailid;
-  // }
-
-
 
 }
