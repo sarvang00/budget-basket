90de8be7c13061683a5558225fa30bada566d535
Sarvang Dave <sarvang00@gmail.com>
2023-03-24 14:55:51 -0300

    Removing component that no longer requires testing

--- BudgetBasket/src/app/finmanager/order/order.component.spec.ts
+++ BudgetBasket/src/app/finmanager/order/order.component.spec.ts
@@ -1,23 +1,2 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignUpComponent } from './order.component';
-
-describe('SignUpComponent', () => {
-  let component: SignUpComponent;
-  let fixture: ComponentFixture<SignUpComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(SignUpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
