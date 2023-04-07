5bf0d63249aa614f563f23986ee3bb6b91cae8c3
Jayant Patidar <jy862746@dal.ca>
2023-04-07 19:33:44 -0300

    changed cross origin

--- backend/src/main/java/com/dal/PFE/controller/UserController.java
+++ backend/src/main/java/com/dal/PFE/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("user")
 public class UserController {
 
