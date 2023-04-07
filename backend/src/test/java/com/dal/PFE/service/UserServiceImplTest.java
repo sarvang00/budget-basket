7749d79c3e4a69c511dc43787c1d67fd96430890
mansi <mansi.v@turabit.com>
2023-04-07 18:43:41 -0300

    Data test added

--- backend/src/test/java/com/dal/PFE/service/UserServiceImplTest.java
+++ backend/src/test/java/com/dal/PFE/service/UserServiceImplTest.java
@@ -3,12 +3,16 @@ package com.dal.PFE.service;
 import com.dal.PFE.dao.UserDao;
 import com.dal.PFE.model.User;
 
+import com.dal.PFE.model.UserAddress;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,6 +35,16 @@ public class UserServiceImplTest {
     }
 
     @Test
+    public void testRegisterUserWithData() throws Exception {
+        Date date = new Date();
+        List<UserAddress> userAddresses = new ArrayList<UserAddress>();
+        User user = new User(2, "Developer", "Spring", "dev@gmail.com", "Dev@1234", "987654321", date,date,userAddresses);
+        userService.registerUser(user);
+        verify(userDao, times(1)).registerUser(user);
+    }
+
+
+    @Test
     public void testGetUsers() throws Exception {
         List<User> userList = new ArrayList<>();
         when(userDao.getUsers()).thenReturn(userList);
@@ -50,6 +64,15 @@ public class UserServiceImplTest {
 
     @Test
     public void testUpdateUser() throws Exception {
+        Date date = new Date();
+        List<UserAddress> userAddresses = new ArrayList<UserAddress>();
+        User user = new User(2, "Developer", "Spring", "dev@gmail.com", "Dev@1234", "987654321", date,date,userAddresses);
+        userService.updateUser(user);
+        verify(userDao, times(1)).updateUser(user);
+    }
+
+    @Test
+    public void testUpdateUserWithData() throws Exception {
         User user = new User();
         userService.updateUser(user);
         verify(userDao, times(1)).updateUser(user);
